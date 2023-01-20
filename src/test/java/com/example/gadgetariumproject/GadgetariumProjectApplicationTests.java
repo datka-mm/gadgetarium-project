38342759cfc8ad914f436d0e647c3e77afd1c259
Datka <datkascnu@gmail.com>
2023-01-20 15:10:44 +0600

    project

---
+++ src/test/java/com/example/gadgetariumproject/GadgetariumProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gadgetariumproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetariumProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
