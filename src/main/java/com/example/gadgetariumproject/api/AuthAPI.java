623df786661070849dcf77a5d71db8f251466be1
Datka <datkascnu@gmail.com>
2023-01-24 19:19:43 +0600

    sign in method finished

--- src/main/java/com/example/gadgetariumproject/api/AuthAPI.java
+++ src/main/java/com/example/gadgetariumproject/api/AuthAPI.java
@@ -2,6 +2,7 @@ package com.example.gadgetariumproject.api;
 
 import com.example.gadgetariumproject.db.service.UserService;
 import com.example.gadgetariumproject.dto.request.SighUpRequest;
+import com.example.gadgetariumproject.dto.request.SignInRequest;
 import com.example.gadgetariumproject.dto.response.AuthResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -18,8 +19,14 @@ public class AuthAPI {
     private final UserService userService;
 
     @Operation(summary = "Sign up", description = "Any user can register")
-    @PostMapping
+    @PostMapping("/registration")
     public AuthResponse registration(@RequestBody SighUpRequest authRequest) {
         return userService.registration(authRequest);
+    }
+
+    @Operation(summary = "Sign in", description = "Login")
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody SignInRequest request) {
+        return userService.login(request);
     }
 }
--- src/main/java/com/example/gadgetariumproject/db/service/UserService.java
+++ src/main/java/com/example/gadgetariumproject/db/service/UserService.java
@@ -4,10 +4,12 @@ import com.example.gadgetariumproject.config.jwt.JwtTokenUtil;
 import com.example.gadgetariumproject.db.model.User;
 import com.example.gadgetariumproject.db.repository.UserRepository;
 import com.example.gadgetariumproject.dto.request.SighUpRequest;
+import com.example.gadgetariumproject.dto.request.SignInRequest;
 import com.example.gadgetariumproject.dto.response.AuthResponse;
 import com.example.gadgetariumproject.enums.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -33,6 +35,31 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(sighUpRequest.getPassword()));
         user.setRole(Role.USER);
         userRepository.save(user);
+
+        String jwtToken = jwtUtil.generateToken(user.getEmail());
+        return new AuthResponse(
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getPhoneNumber(),
+                user.getEmail(),
+                jwtToken
+        );
+    }
+
+    public AuthResponse login(SignInRequest request) {
+        User user = userRepository.findByEmail(request.getEmail()).orElseThrow(
+                () -> new UsernameNotFoundException(String.format("User with email: %s not found!", request.getEmail())
+                )
+        );
+
+        if (request.getPassword().isBlank()) {
+            throw new BadRequestException("Password can't be empty!");
+        }
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new BadCredentialsException("Incorrect password!");
+        }
 
         String jwtToken = jwtUtil.generateToken(user.getEmail());
         return new AuthResponse(
