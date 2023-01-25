9b9b4e9054d145469e89e2bc808c2942bdb669c7
Datka <datkascnu@gmail.com>
2023-01-25 12:33:29 +0600

    added ExceptionResponse class

--- src/main/java/com/example/gadgetariumproject/exceptions/handler/GlobalException.java
+++ src/main/java/com/example/gadgetariumproject/exceptions/handler/GlobalException.java
@@ -1,4 +1,10 @@
 package com.example.gadgetariumproject.exceptions.handler;
 
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalException {
+
+    public GlobalException() {
+    }
 }
