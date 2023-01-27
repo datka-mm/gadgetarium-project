30d04023dd889f3c2df79e121dadffb5945a5f28
Datka <datkascnu@gmail.com>
2023-01-27 17:42:10 +0600

    added ProductRepository class and added some methods in ProductService class

--- src/main/java/com/example/gadgetariumproject/db/service/ProductService.java
+++ src/main/java/com/example/gadgetariumproject/db/service/ProductService.java
@@ -1,5 +1,8 @@
 package com.example.gadgetariumproject.db.service;
 
+import com.example.gadgetariumproject.dto.request.ProductRequest;
+import com.example.gadgetariumproject.dto.response.ProductResponse;
+import com.example.gadgetariumproject.dto.response.SimpleResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +12,19 @@ import javax.transaction.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponse addProduct(ProductRequest productRequest) {
+
+    }
+
+    public ProductResponse getProductById(Long id) {
+
+
+    }
+
+    public SimpleResponse deleteProductById(Long id) {
+
+    }
 }
