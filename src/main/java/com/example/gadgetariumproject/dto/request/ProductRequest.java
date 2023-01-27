feca16a79fafcce2157bfe701181dd4f4eed0347
Datka <datkascnu@gmail.com>
2023-01-27 17:36:35 +0600

    added ProductRequest class

---
+++ src/main/java/com/example/gadgetariumproject/dto/request/ProductRequest.java
@@ -0,0 +1,11 @@
+package com.example.gadgetariumproject.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductRequest {
+
+    private String name;
+}
