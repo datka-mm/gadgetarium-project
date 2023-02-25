174f1b0c00bfbee9f1cafb77a763d4c06ec40905
Datka <datkascnu@gmail.com>
2023-02-25 13:29:24 +0600

    added categoryRequest

---
+++ src/main/java/com/example/gadgetariumproject/dto/request/CategoryRequest.java
@@ -0,0 +1,11 @@
+package com.example.gadgetariumproject.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryRequest {
+
+    private String name;
+}
