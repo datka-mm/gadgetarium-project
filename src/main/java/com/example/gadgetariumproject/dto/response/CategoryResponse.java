4133193378a0ede37c9e3ab8716df74a68b2d2c9
Datka <datkascnu@gmail.com>
2023-02-25 13:36:41 +0600

    added CategoryResponse

---
+++ src/main/java/com/example/gadgetariumproject/dto/response/CategoryResponse.java
@@ -0,0 +1,14 @@
+package com.example.gadgetariumproject.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CategoryResponse {
+
+    private Long id;
+    private String name;
+}
