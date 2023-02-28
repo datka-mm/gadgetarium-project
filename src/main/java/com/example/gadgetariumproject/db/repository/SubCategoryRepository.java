b634614f7d0cec3ff128d5fef2337e51e30c17e5
Datka <datkascnu@gmail.com>
2023-02-28 16:12:31 +0600

    added SubCategoryService class

--- src/main/java/com/example/gadgetariumproject/db/repository/SubCategoryRepository.java
+++ src/main/java/com/example/gadgetariumproject/db/repository/SubCategoryRepository.java
@@ -2,6 +2,8 @@ package com.example.gadgetariumproject.db.repository;
 
 import com.example.gadgetariumproject.db.model.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
 }
