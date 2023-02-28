5940b4840cb5de5c732ca97a7ecf1a9a12357bb8
Datka <datkascnu@gmail.com>
2023-02-28 19:13:10 +0600

    added something

--- src/main/java/com/example/gadgetariumproject/db/repository/SubCategoryRepository.java
+++ src/main/java/com/example/gadgetariumproject/db/repository/SubCategoryRepository.java
@@ -1,9 +1,19 @@
 package com.example.gadgetariumproject.db.repository;
 
 import com.example.gadgetariumproject.db.model.SubCategory;
+import com.example.gadgetariumproject.dto.response.SubCategoryResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
+
+//    @Query("select new com.example.gadgetariumproject.dto.response.SubCategoryResponse(" +
+//            "s.id, " +
+//            "s.name)" +
+//            "from SubCategory s where s.")
+//    List<SubCategoryResponse> getAll(Long categoryId);
 }
