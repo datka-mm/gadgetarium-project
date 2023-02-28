af2a8d23679ba3ee98ac04b3ab269aa37933797d
Datka <datkascnu@gmail.com>
2023-02-28 17:23:26 +0600

    added SubCategoryResponse class

--- src/main/java/com/example/gadgetariumproject/api/SubCategoryAPI.java
+++ src/main/java/com/example/gadgetariumproject/api/SubCategoryAPI.java
@@ -1,13 +1,12 @@
 package com.example.gadgetariumproject.api;
 
+import com.example.gadgetariumproject.db.model.SubCategory;
 import com.example.gadgetariumproject.db.service.SubCategoryService;
+import com.example.gadgetariumproject.dto.request.SubCategoryRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/sub-category")
@@ -20,6 +19,9 @@ public class SubCategoryAPI {
 
     @Operation(summary = "Add sub category", description = "Add new sub category")
     @PostMapping
+    public SubCategoryResponse addSubCategory(@RequestBody SubCategoryRequest request) {
+        return service.addSubCategory(request);
+    }
 
 
 }
---
+++ src/main/java/com/example/gadgetariumproject/dto/response/SubCategoryResponse.java
@@ -0,0 +1,13 @@
+package com.example.gadgetariumproject.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubCategoryResponse {
+}
