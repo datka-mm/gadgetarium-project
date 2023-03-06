66c873f76f7c386664419cf776a065958ee8b227
Datka <datkascnu@gmail.com>
2023-03-06 16:28:11 +0600

    added CommentRepository and CommentService classes

---
+++ src/main/java/com/example/gadgetariumproject/db/service/CommentService.java
@@ -0,0 +1,14 @@
+package com.example.gadgetariumproject.db.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CommentService {
+
+
+}
